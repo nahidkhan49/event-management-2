0a3515effe5eb10f206a8a24a7e9e64c90d368f2
Nahid <nahid822399@gmail.com>
2026-01-23 19:27:58 +0600

    add debug toolbar

--- event_management/settings.py
+++ event_management/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'event',
+    'debug_toolbar',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'event_management.urls'
@@ -125,4 +127,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
+
 
--- event_management/urls.py
+++ event_management/urls.py
@@ -1,10 +1,12 @@
-
+from debug_toolbar.toolbar import debug_toolbar_urls
 from django.contrib import admin
 from django.urls import path,include
 from event.views import home
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("event/", include("event.urls")),
     path('',home,name='home'),
-]
+]+debug_toolbar_urls()
