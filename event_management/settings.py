64e1039554e9c4cb0fc383951af7232e0a5dd7a6
Nahid <nahid822399@gmail.com>
2026-03-12 14:01:33 +0600

    project update

--- event_management/settings.py
+++ event_management/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 
 from pathlib import Path
 from decouple import config
+import dj_database_url
+import os
+
 
 
 
@@ -85,17 +88,28 @@ WSGI_APPLICATION = 'event_management.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'event',
-        'USER': 'postgres',
-        'PASSWORD': 'nahidn95',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'event',
+#         'USER': 'postgres',
+#         'PASSWORD': 'nahidn95',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
+
+import dj_database_url
+import os
+
+DATABASES = {
+    "default": dj_database_url.config(
+        default=os.environ.get("DATABASE_URL"),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 
 # Password validation
