f230bbe68f536dffe4a584b2f425570aa713a464
Nahid <nahid822399@gmail.com>
2026-04-04 22:43:27 +0600

    Fix email back

--- users/signals.py
+++ users/signals.py
@@ -6,23 +6,23 @@ from django.core.mail import send_mail
 from django.conf import settings
 from urllib.parse import quote
 
+
 from django.utils.http import urlsafe_base64_encode
 from django.utils.encoding import force_bytes
 
 
 
 @receiver(post_save, sender=User)
-def send_activation_email(sender,instance,created,**kwargs):
+def send_activation_email(sender, instance, created, **kwargs):
     if created:
         uid = urlsafe_base64_encode(force_bytes(instance.pk))
         token = default_token_generator.make_token(instance)
-        from urllib.parse import quote
 
-        activation_link = f"https://event-management-2-3zlo.onrender.com/users/activate/{uid}/{quote(token)}"
-        
+        activation_link = f"https://event-management-2-3zlo.onrender.com/users/activate/{uid}/{token}/"
+
         send_mail(
-            subject='Activation Your account',
-            message=f'Hello {instance.username},\nClick to activate:\n{activation_link}',
+            subject='Activate Your Account',
+            message=f'Hello {instance.username},\n\nClick to activate your account:\n{activation_link}',
             from_email=settings.EMAIL_HOST_USER,
             recipient_list=[instance.email],
             fail_silently=False
